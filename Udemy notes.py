79071a8b19605d5edb8a3cfe0cd033bdbb1de8de
apolot99 <44310628+apolot99@users.noreply.github.com>
2020-06-22 16:29:29 -0400

    Create Udemy notes.py

---
+++ Udemy notes.py
@@ -0,0 +1,31 @@
+#           PYTHON 2 VERSUS PYTHON 3
+# 99% of external packages that you download support python 3 and some are made exclusively for python 3 e.g django for web dev't
+# If you know python 3, you already know python 2. You just need to learn a few syntax changes.
+
+#           COMMAND LINE BASICS
+# The command line allows you to programmatically move through your computer's directories.
+#  cd-find your current directory
+#  dir-list all files in a directory
+#  cd (name of a folder)-if I want to move into a specific folder
+#  cd .. - if I want to move up a directory
+#  cls-clear entire command line screen
+# Hidden files in a directory are denoted starting off with a dot
+
+#           INSTALLING PYTHON
+# Advantages of using python
+#  -ability to learn it quickly
+#  -involves less code
+#  -syntax is easier to read(uses indentation instead of curly braces)
+#  -utilized by every major technology company
+#  -huge amount of open source libraries
+#  -has standard libraries included
+# Anaconda distribution includes python as well as many other useful libraries like jupiter
+# Anaconda can be installed onto any major operationg system
+# Types of environments (where you're gonna type your python code)
+#  -Text Editors
+#  -Full IDEs (Intergrated Developement Environment); designed specifically for python e.g PyCharm & Spyder
+#  -Notebook environments (runs code on a cell based system)
+
+#          GIT AND GITHUB OVERVIEW
+# Github is a website where you can publish your git repositories and collaborate with other developers
+# snapshots of your code on Github are known as commits
