2ceb8e6143387527369ace82eeade064b6a878ae
apolot99 <44310628+apolot99@users.noreply.github.com>
2020-06-24 12:20:00 -0400

    Update Udemy notes.py

--- Udemy notes.py
+++ Udemy notes.py
@@ -29,3 +29,44 @@
 #          GIT AND GITHUB OVERVIEW
 # Github is a website where you can publish your git repositories and collaborate with other developers
 # snapshots of your code on Github are known as commits
+
+#         PYTHON DATA TYPES
+# -Integers (whole numbers)
+# -Floating point (whole numbers with decimals)
+# -Strings (syntax of characters using either single or double quotes e.g "hello world")
+# -Lists
+# -Dictionaries
+# -Tuples
+# -Sets
+# -Booleans
+
+# use the mod opeartor(%) to find out the remainder from dividing.
+# If mod 2 results in anything other than zero then that number is odd.
+
+#  RULES FOR CHOOSING VARIABLE NAMES IN PYTHON
+#    -names can't start with a number
+#    -there can be no spaces in the name instead use under score
+#    -can't use any of these signs ("",<>/?|\!@#$%^&*~-+)
+#    -names should be lower case
+#    -avoid using words that have special meaning in python like 'list','str'
+
+# python uses dynamic typing whihc means you can reassign varibales to different data types
+
+# Indexing means when you want to grab a single character form a string
+# indexing notation uses [] after the string
+# Every single character has an index assignes to it
+# Indexing starts at zero
+# Reverse indexing helps you grab the last letter of a string however long that string is by just grabbing negavtive one 
+
+# slicing allows you to grab a subsection of multiple characters i.e a slice of a string
+# slicing has the following syntax; [start:stop:step]
+#   start-is a numerical index for the slice start
+#   stop-is the index you will go up to but not include
+#   step-is the size of the 'jump' you take 
+
+#     PROPERTIES OF STRINGS
+# -Can not mutate or change
+
+# numbers in strings can not be added.
+
+# String interpolation means injectinf a varibale in to a string.
